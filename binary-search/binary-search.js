ec4c26619d0f84b2a0078389d8de9c5f697f1e43
Linwei Liu <llwsgr@hotmail.com>
2020-04-08 15:27:36 -0400

    binary search WIP

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,24 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let midIndex = Math.floor(array.length/2);
+	let midValue = array[midIndex];
+	if(target < midValue){
+		array = array.slice(0, midIndex);
+		if(array.length ===1){
+			if(array[0]=== target) return true;
+		} else{
+			binarySearch(array, target);
+		}
+	} else {
+		array = array.slice(midIndex);
+		if(array.length ===1){
+			if(array[0]=== target) return true;
+		} else{
+			binarySearch(array, target);
+		}
+	}
+	return false;
 };
 
 /*
